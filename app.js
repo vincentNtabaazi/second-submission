10a515b02e1ecd1833ae2a4ad2abaf08c32b897f
vincentNtabaazi <vincentntabaazi02@gmail.com>
2023-06-29 19:13:46 +0300

    javascript function to add functionality to the + button

---
+++ app.js
@@ -0,0 +1,28 @@
+// Selectors
+const todoInput =document.querySelector('.user-todo-input ')
+const todoButton =document.querySelector('.user-todo-button ')
+const todoList =document.querySelector('.user-todo-list ') 
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+
+//Functions
+function addTodo(event){
+  event.preventDefault();
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "hey";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = "<i class='fas fa-check'></i>";
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = "<i class='fas fa-trash'></i>";
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  todoList.appendChild(todoDiv);
+}
